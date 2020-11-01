6fabddae46293c002959045be05ed05696bd5c8b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-11-02 05:34:43 +0600

    added blogpage component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from './components/Home/Home/Home';
 import About from './components/About/About';
 import Work from './components/Work/Work';
 import ContactPage from './components/ContactPage/ContactPage';
+import BlogPage from './components/BlogPage/BlogPage';
 
 function App() {
   return (
@@ -29,6 +30,10 @@ function App() {
 
           <Route path="/contact">
             <ContactPage/>
+          </Route>
+
+          <Route path="/blog">
+            <BlogPage/>
           </Route>
         </Switch>
       </div>
